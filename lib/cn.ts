ecab52f9430fdb751db08faaa80c87b70c03dc23
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-07-28 00:51:31 -0400

    Create cn.ts
    this can be called utils.ts

---
+++ lib/cn.ts
@@ -0,0 +1,6 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+} // fnc to make things work w/ Tailwind
